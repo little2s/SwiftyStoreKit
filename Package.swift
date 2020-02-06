fca736f7eb3519f0f33fe77da904dc97545c68f5
little2s <duanyinglun@gmail.com>
2020-02-06 11:37:53 +0800

    fix warn

--- Package.swift
+++ Package.swift
@@ -1,16 +1,21 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "SwiftyStoreKit",
-    // platforms: [.iOS("8.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
+    platforms: [.iOS(.v10),.macOS(.v10_13)],
     products: [
-        .library(name: "SwiftyStoreKit", targets: ["SwiftyStoreKit"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftyStoreKit",
+            targets: ["SwiftyStoreKit"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyStoreKit",
-            path: "SwiftyStoreKit"
-        )
+            path: "SwiftyStoreKit"),
     ]
 )
